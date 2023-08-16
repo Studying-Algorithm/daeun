879632c4cdc24097e7aedde65c254bd42df93510
namdaeun <nde40345@gmail.com>
2023-08-16 20:07:32 +0900

    "[Solve] BJ_2588"

--- 2588.py
+++ 2588.py
@@ -1,4 +1,4 @@
-num1 = int(input())
+num1 = int(input()) 
 num2 = int(input())
 
 num2_1 = num2 % 10 # 일의 자리 수
@@ -8,6 +8,7 @@ num2_100 = num2 // 100 # 백의 자리 수
 res1 = num1 * num2_1
 res2 = num1 * num2_10  
 res3 = num1 * num2_100 
+
 # 더해지는 자릿수를 맞추기 위해 10, 100 곱해서 더함
 num_sum = res1 + (res2 * 10) + (res3 * 100)
 print(res1)
